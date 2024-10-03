678a30a24b19e38ae41616594b3130e44e426dd8
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-03 17:51:33 +0200

    Implement description enums

--- Descriptions.cs
+++ Descriptions.cs
@@ -1,6 +1,28 @@
 ﻿namespace OOPArv;
 
-public enum Descriptions
+public enum Color
 {
-    
+    Black,
+    White,
+    Blue,
+    Orange,
+    Yellow,
+    Pink,
+    Red,
+    Gold,
+    Silver,
+    Brown,
+    Beige,
+    Green,
+    Purple,
+    Gray,
+}
+
+public enum Size
+{
+    Small,
+    Skinny,
+    Big,
+    Fat,
+    Muscular,
 }
