b160ab00d199de110c01e044a207dda145c086c9
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-07 10:43:57 +0200

    Add classes to file

--- AnimalTypes.cs
+++ AnimalTypes.cs
@@ -1,6 +1,30 @@
 ﻿namespace OOPArv;
 
-public class AnimalTypes
+public class Human(string name, int age, Color color)
+    : Animal(AnimalSpecies.Human, name, age, color)
 {
+    public void DisplayInformation()
+    {
+        Console.WriteLine();
+    }
     
+    public void Greet()
+    {
+        Console.WriteLine($"{Name} says hello!");
+    }
+    
+    public override void Sleep()
+    {
+        Console.WriteLine($"{Name} is tired and goes home to sleep...");
+    }
+}
+
+public abstract class NonHuman(AnimalSpecies species, string name, int age, Color color, bool likesHumans)
+    : Animal(species, name, age, color)
+{
+    protected bool LikesHumans { get; set; } = likesHumans;
+
+    public abstract void DisplayAnimal();
+
+    public abstract void Pet();
 }
