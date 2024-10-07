315ec037c4d481ea03d7b8be1d28856cf7da2003
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-07 09:44:41 +0200

    Add methods DisplayAnimal and Sleep

--- Human.cs
+++ Human.cs
@@ -3,8 +3,19 @@
 public class Human(string name, int age, Color color, bool likesOtherHumans)
     : Animal(AnimalSpecies.Human, name, age, color, likesOtherHumans)
 {
+    public override void DisplayAnimal()
+    {
+        Console.WriteLine($"{Name} is {Age} years old and has {Color} skin color." );
+    }
+    
+    public override void Sleep()
+    {
+        Console.WriteLine($"{Name} is tired and goes home to sleep...");
+    }
+    
     public void Greet()
     {
         Console.WriteLine($"{Name} says hello!");
     }
+    
 }
