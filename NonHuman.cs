21a957bc8b49da7f478188dbde00d345ea398e9c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-07 10:24:03 +0200

    Add constructor that inherits from Animal class

--- NonHuman.cs
+++ NonHuman.cs
@@ -3,4 +3,20 @@
 public class NonHuman : Animal, INonHuman
 {
     public bool LikesHumans { get; set; }
+
+    public NonHuman(AnimalSpecies species, string name, int age, Color color, bool likesHumans) :
+        base (species, name, age, color)
+    {
+        LikesHumans = likesHumans;
+    }
+
+    public virtual void DisplayAnimal()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Pet()
+    {
+        throw new NotImplementedException();
+    }
 }
