a7fce8bae6527d1b02329f025559c02c6dd5ea68
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-07 16:52:45 +0200

    Change test code for Program.cs

--- Program.cs
+++ Program.cs
@@ -13,13 +13,16 @@ class Program
         Elephant elephant = new Elephant("Babar", 7, Color.Gray, true, 7000);
         Zebra zebra = new Zebra("Super Zebran", 3, Color.White, true, 500);
         Wolf wolf = new Wolf("Balto", 10, Color.Gray, true, Color.Gold);
+        Crocodile crocodile = new Crocodile("Snapper", 20, Color.Green, 500);
+        Salamander salamander = new Salamander("Sally", 2, Color.Red, true, false);
         Puppy puppy = new Puppy("Asma", 1, Color.Black, false, Color.Brown, Size.Small);
         
-        leopard.Climb();
-        tiger.SharpenClaws();
-        elephant.SprayWater();
-        zebra.RotateEars();
-        wolf.GatherPack();
-        puppy.DisplaySize();
+        crocodile.DisplayAnimal();
+        crocodile.Pet();
+        
+        salamander.DisplayAnimal();
+        salamander.CheckTail();
+        salamander.RegenerateTail();
+        salamander.CheckTail();
     }
 }
