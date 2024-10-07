9e9419578edf8eb6faea62001f4352ca73d59287
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-07 09:53:18 +0200

    Remove DisplayAnimal method

--- Human.cs
+++ Human.cs
@@ -3,11 +3,6 @@
 public class Human(string name, int age, Color color, bool likesOtherHumans)
     : Animal(AnimalSpecies.Human, name, age, color, likesOtherHumans)
 {
-    public override void DisplayAnimal()
-    {
-        Console.WriteLine($"{Name} is {Age} years old and has {Color} skin color." );
-    }
-    
     public override void Sleep()
     {
         Console.WriteLine($"{Name} is tired and goes home to sleep...");
