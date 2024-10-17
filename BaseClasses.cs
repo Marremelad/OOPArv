dba27f154d326d06855223d242b52c37593bf774
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-17 17:09:13 +0200

    Add text to DisplayInfo method in Human class

--- BaseClasses.cs
+++ BaseClasses.cs
@@ -81,7 +81,7 @@ public abstract class Human(string name, int age, Color color)
     
     public void DisplayInformation()
     {
-        Console.WriteLine();
+        Console.WriteLine($"{Name} is {Age} years old and has {color} skin.");
     }
 
     public void Greet()
