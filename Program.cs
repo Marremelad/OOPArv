535073a51f814c2f7fc01cb837d43d023c639459
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-17 17:34:39 +0200

    changed test code

--- Program.cs
+++ Program.cs
@@ -8,6 +8,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        List<NonHuman> listOfAnimals =
+        [
+            new Leopard("Lucas", 10, Color.Yellow, 500),
+            // new Tiger("Tony", 5, Color.Orange, 300),
+            new Elephant("Erik", 20, Color.Gray, true, 5000),
+            // new Zebra("Zack", 15, Color.White, 55),
+            // new Wolf("Wayne", 7, Color.Black, false, Color.Blue),
+            // new Puppy("Pall", 1, Color.Brown, true, Color.Gold, Size.Fat),
+            // new Crocodile("Cain", 19, Color.Green, 100),
+            // new Salamander("Sally", 2, Color.Red, true, false),
+        ];
+
+        foreach (var animal in listOfAnimals)
+        {
+            if (animal is IWild wildAnimal) wildAnimal.RoamWild();
+            else if (animal is ITame tameAnimal) tameAnimal.TryPet();
+            
+            animal.DisplayAnimalType();
+            animal.DisplayAnimal();
+            animal.Sleep();
+            animal.MakeSound();
+            animal.Unique();
+        }
     }
 }
